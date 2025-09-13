5ac7af84388d622aff1275827c978efc019ed57a
William Dale <wd40bug@gmail.com>
2025-09-13 16:54:16 -0500

    File Upload

--- Colors.cs
+++ Colors.cs
@@ -5,7 +5,8 @@ namespace BlazorTest
         public const string rose = "#ff006eff";
         public const string hunyadi = "#edae49ff";
         public const string mint = "#8DCE9E";
-        public const string purple = "#7A28CB";
+        public const string purple = "#9955DD";
+        public const string light_purple = "#CCAAEE";
         public const string mimi = "#ffe1eaff";
         public const string lichoriche = "#22181C";
     }
